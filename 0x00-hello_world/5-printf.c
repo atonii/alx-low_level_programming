017a0a93ba1a739a571ec96d4e0a9906b9504a85
atonii <atoni.melh@gmail.com>
2023-07-14 01:15:39 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main - entry point
+ * Description: print strings using printf function
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
