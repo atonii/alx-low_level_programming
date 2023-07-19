466681c5624f2890528a81dc6a640f0f7b71318b
atonii <atoni.melh@gmail.com>
2023-07-19 07:44:35 -0700

    ./10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - function name
+ * @a: left operand
+ * @b: right operand
+ * Description: adds bith operands
+ * Return: return sum
+ */
+
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
