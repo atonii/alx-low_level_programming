9fda68681b696498a4c1bc03c3fef8c90bb70064
atonii <atoni.melh@gmail.com>
2023-07-21 13:19:49 -0700

    introduced another n++

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_numbers - function name
+ * Description: prints from 0 - 9 except 2 and 4
+ * Return: returns nothing
+ */
+
+void print_most_numbers(void)
+{
+	int n;
+
+	n = 0;
+	while (n < 10)
+	{
+		if ((n == 2) || (n == 4))
+		{
+			n++;
+		}
+		_putchar('0' + n);
+		n++;
+	}
+	_putchar('\n');
+}
