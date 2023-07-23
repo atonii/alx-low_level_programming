5d536f49c7943cf8dbc1974686e088a53458a11d
atonii <atoni.melh@gmail.com>
2023-07-23 14:37:27 -0700

    fixed february days issue

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -1,4 +1,4 @@
- #include <stdio.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -13,9 +13,9 @@ int main(void)
     int day;
     int year;
 
-    month = 03;
-    day = 1;
-    year = 2008;
+    month = 2;
+    day = 29;
+    year = 2000;
 
     printf("Date: %02d/%02d/%04d\n", month, day, year);
 
