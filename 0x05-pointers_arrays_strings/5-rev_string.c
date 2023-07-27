b69ebb88de5d38065cd65455d331ed0b743c4215
atonii <atoni.melh@gmail.com>
2023-07-27 08:21:12 -0700

    function to rev string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rev_string - print string in reverse
+ * @s: pointer to string
+ */
+
+void rev_string(char *s)
+{
+	int old_index, new_index;
+	char temp = 0;
+
+	old_index = 0;
+	new_index = 0;
+
+	while (s[++old_index] != '\0');
+	old_index--;
+
+	while (new_index < old_index)
+	{
+		temp = s[new_index];
+		s[new_index] = s[old_index];
+		s[old_index] = temp;
+
+		new_index++;
+		old_index--;
+	}
+
+}
