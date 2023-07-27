1e4c92c859717551e4c30d38bd50772f28b23d0f
atonii <atoni.melh@gmail.com>
2023-07-27 08:47:44 -0700

    Function to print every other char

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other char
+ * starting with first char
+ * @str: pointer to string
+ */
+
+void puts2(char *str)
+{
+	int strlen, new_index;
+
+	strlen = 0;
+	new_index = 0;
+
+	while (str[strlen++] != '\0')
+		;
+
+	while (new_index < strlen)
+	{
+		_putchar(str[new_index]);
+		new_index += 2;
+	}
+	_putchar('\n');
+
+}
