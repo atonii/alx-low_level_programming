73e15cb2db7486977479afc07dd34283ab9d173e
atonii <atoni.melh@gmail.com>
2023-07-27 09:23:23 -0700

    print second half of string

--- 0x05-pointers_arrays_strings/7-main.c
+++ 0x05-pointers_arrays_strings/7-main.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  * main - check the code
  *
@@ -9,7 +9,7 @@ int main(void)
 {
     char *str;
 
-    str = "\0a0123456789";
-    puts2(str);
+    str = "0123456789";
+    puts_half(str);
     return (0);
 }
---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * puts_half - prints second half of string
+ * (length_of_the_string -1) / 2 odd
+ * @str: pointer to string
+ */
+
+void puts_half(char *str)
+{
+	int strlen, new_index;
+
+	strlen = 0;
+	new_index = 0;
+
+	while (str[strlen++] != '\0')
+		;
+
+	strlen--;
+	
+	if (strlen % 2 != 0)
+		new_index = (strlen - 1) / 2;
+	new_index = strlen / 2;
+
+	while (new_index < strlen)
+	{
+		_putchar(str[new_index]);
+		new_index++;
+	}
+	_putchar('\n');
+
+}
