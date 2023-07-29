a3839ff90d972b4c5de75e98619422fec13a8876
atonii <atoni.melh@gmail.com>
2023-07-29 10:48:37 +0000

    str compare function

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcmp - function that compares strings
+ * @s1: first string
+ * @s2: second string
+ * Return: returns 0 if equal, -15 if s1 < s2 15 if s1 > s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] == s2[i])
+			i++;
+		else if (s1[i] < s2[i])
+			return (-15);
+		else
+			return (15);
+	}
+	return (0);
+}
